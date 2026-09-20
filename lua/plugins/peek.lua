748c6516d99be3acda49a9181bec76c2f34d704f
CrunchyToothpicks100 <wolfywoofmc@gmail.com>
2026-09-20 17:29:14 -0500

    Clean up plugin loading configuration

--- lua/plugins/peek.lua
+++ lua/plugins/peek.lua
@@ -1,28 +1,28 @@
-local function gh(repo) return "https://github.com/" .. repo end
+local function gh(repo) return 'https://github.com/' .. repo end
 
-vim.pack.add({
-    { src = gh("toppair/peek.nvim") },
-})
+vim.pack.add {
+  { src = gh 'toppair/peek.nvim' },
+}
 
-local peek = require("peek")
+local peek = require 'peek'
 
-peek.setup({
-    auto_load = true,
-    close_on_bdelete = true,
+peek.setup {
+  auto_load = true,
+  close_on_bdelete = true,
 
-    -- Use the system opener directly so WSL routes through xdg-open.
-    app = { "xdg-open" },
+  -- Use the system opener directly so WSL routes through xdg-open.
+  app = { 'xdg-open' },
 
-    syntax = true,
-    theme = "dark",
+  syntax = true,
+  theme = 'dark',
 
-    update_on_change = true,
+  update_on_change = true,
 
-    filetype = { "markdown" },
+  filetype = { 'markdown' },
 
-    throttle_at = 200000,
-    throttle_time = "auto",
-})
+  throttle_at = 200000,
+  throttle_time = 'auto',
+}
 
-vim.api.nvim_create_user_command("PeekOpen", peek.open, {})
-vim.api.nvim_create_user_command("PeekClose", peek.close, {})
+vim.api.nvim_create_user_command('PeekOpen', peek.open, {})
+vim.api.nvim_create_user_command('PeekClose', peek.close, {})
