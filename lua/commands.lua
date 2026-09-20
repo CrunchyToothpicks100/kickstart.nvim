9b67ec48ba4d488ca53bbf9aa2ac64691f8ff36c
CrunchyToothpicks100 <wolfywoofmc@gmail.com>
2026-09-20 17:29:09 -0500

    Clean up commands and options

--- lua/commands.lua
+++ lua/commands.lua
@@ -1,15 +1,7 @@
 -- List Plugins
-vim.api.nvim_create_user_command(
-  'Plugins',
-  function ()
-    print (
-      table.concat(
-        vim.iter(vim.pack.get())
-          :map(function (x) return x.spec.name end)
-          :totable(),
-        '\n'
-      )
-    )
-  end,
-  {}
-)
+vim.api.nvim_create_user_command('Plugins', function()
+  print(table.concat(vim.iter(vim.pack.get()):map(function(x) return x.spec.name end):totable(), '\n'))
+end, {})
+
+-- LspInfo alias
+vim.api.nvim_create_user_command('LspInfo', 'checkhealth vim.lsp', {})
--- lua/options.lua
+++ lua/options.lua
@@ -6,24 +6,21 @@ vim.o.shiftwidth = 2
 vim.o.expandtab = true
 
 vim.o.undofile = true
-vim.o.undodir = vim.fn.expand('~/.config/nvim/undofiles')
+vim.o.undodir = vim.fn.expand '~/.config/nvim/undofiles'
 
 vim.o.backupcopy = 'yes'
 
-vim.cmd.colorscheme('tokyonight-night')
+vim.cmd.colorscheme 'tokyonight-night'
 
 -- vim.o.background = 'light'
 
 -- Don't auto-continue comments
 vim.api.nvim_create_autocmd('FileType', {
-    pattern = '*',
-    callback = function()
-        vim.opt_local.formatoptions:remove({ 'o', 'r' })
-    end,
+  pattern = '*',
+  callback = function() vim.opt_local.formatoptions:remove { 'o', 'r' } end,
 })
 
 -- Turn off list option for terminals
 vim.api.nvim_create_autocmd('TermOpen', {
   callback = function() vim.wo.list = false end,
 })
-
